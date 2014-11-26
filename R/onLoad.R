f0b2218272c3a08645f5b9259b84488ebc437a62
Alexander Myltsev <alexander.myltsev@phystech.edu>
2014-11-26 19:55:04 +0300

    Replace jinit with jpackage

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,6 +1,4 @@
 .onLoad <- function(libname, pkgname) {
   require(rJava)
-  cp = as.character(sapply(rSlick.jars, 
-                           function(jar) { paste(lib.java.path, .Platform$file.sep, jar, sep="") } ))
-  .jinit(classpath =  cp)
+  .jpackage(pkgname, morePaths=".")
 }
