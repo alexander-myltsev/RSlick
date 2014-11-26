b85c787f5bf5dcb73761a7390461f2d348323412
Alexander Myltsev <alexander.myltsev@phystech.edu>
2014-11-26 19:36:56 +0300

    Implement local loading

--- R/zzz.R
+++ R/zzz.R
@@ -6,9 +6,17 @@ dir.create(lib.java.path, recursive=TRUE, showWarnings=FALSE)
 setwd(lib.java.path)
 rSlick.jars <- c("rSlick-lib-0.1.jar", "mysql-connector-java-5.1.12.jar")
 
+download.sites <- c("file:///home/alex/tmp/", "http://github.com/alexander-myltsev/rSlick/releases/download/v0.1/")
 for ( jarName in rSlick.jars ) {
-  url <- paste("http://github.com/alexander-myltsev/rSlick/releases/download/v0.1/", jarName, sep="")
-  download.file(url, jarName, mode="wb", quiet=FALSE, method="wget")
+  localUrl <- paste(download.sites[1], jarName, sep="")
+  tryCatch({
+    download.file(localUrl, jarName, mode="wb", quiet=FALSE)
+    print(paste("loaded locally:", jarName))
+  }, error = function(e) {
+      remoteUrl <- paste(download.sites[2], jarName, sep="")
+      download.file(remoteUrl, jarName, mode="wb", quiet=FALSE, method="wget")
+    }
+  )
 }
 
 setwd(cwd)
