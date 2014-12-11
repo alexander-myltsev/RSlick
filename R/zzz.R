38ed67c1a00fbc5c51393de6e05f742e81f88c1e
Alexander Myltsev <alexander.myltsev@phystech.edu>
2014-12-11 17:37:36 +0300

    Change tmp path

--- R/zzz.R
+++ R/zzz.R
@@ -8,7 +8,7 @@ dir.create(lib.java.path, recursive=TRUE, showWarnings=FALSE)
 setwd(lib.java.path)
 rslick.jars <- c("RSlick-Scala-0.1.0.jar", "mysql-connector-java-5.1.12.jar", "h2-1.4.182.jar")
 
-download.sites <- c("file:///home/alex/tmp/", "http://github.com/alexander-myltsev/RSlick/releases/download/v0.1.0/")
+download.sites <- c("file:///tmp/", "http://github.com/alexander-myltsev/RSlick/releases/download/v0.1.0/")
 for ( jarName in rslick.jars ) {
   localUrl <- paste(download.sites[1], jarName, sep="")
   tryCatch({
